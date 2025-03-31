c45784974ed4cabe3453abfc6546f27b3ee3f404
Miguel Miceli <miguelmicelipdg8@gmail.com>
2025-03-31 18:17:45 -0300

    formulario completo

--- main.js
+++ main.js
@@ -1,20 +1,75 @@
 import { generateReturnsArray } from "./src/investmentGoals";
-const form = document.getElementById('investiment-form');
 
+const form = document.getElementById('investment-form');
+const clearFormButton = document.getElementById('clear-form')
 
 function renderProgression(evt) {
     evt.preventDefault();
-    const startingAmount = Number(document.getElementById('starting-amount').value);
-    const additionalContribution = Number(document.getElementById('additional-contribution').value);
+    if (document.querySelector('.error')) {
+        return;
+    }
+    const startingAmount = Number(document.getElementById('starting-amount').value.replace(",", "."));
+    const additionalContribution = Number(document.getElementById('additional-contribution').value.replace(",", "."));
     const timeAmount = Number(document.getElementById('time-amount').value);
     const timeAmountPeriod = document.getElementById('time-amount-period').value;
-    const returnRate = Number(document.getElementById('return-rate').value);
-    const returnRatePeriod = document.getElementById('evaluantion-period').value;
-    const taxRate = Number(document.getElementById('tax-rate').value);
+    const returnRate = Number(document.getElementById('return-rate').value.replace(",", "."));
+    const returnRatePeriod = document.getElementById('evaluation-period').value;
+    const taxRate = Number(document.getElementById('tax-rate').value.replace(",", "."));
 
-   const returnsArray = generateReturnsArray(startingAmount, timeAmount, timeAmountPeriod, additionalContribution, returnRate, returnRatePeriod);
+    const returnsArray = generateReturnsArray(startingAmount, timeAmount, timeAmountPeriod, additionalContribution, returnRate, returnRatePeriod);
 
-   console.log(returnsArray);
-}  
+    console.log(returnsArray);
+}
+
+function clearform() {
+    form['starting-amount'].value = '';
+    form['additional-contribution'].value = '';
+    form['time-amount'].value = '';
+    form['return-rate'].value = '';
+    form['tax-rate'].value = '';
+
+  const errorInputsContainers =  document.querySelectorAll('.error');
+
+  for (const errorInputContainer of errorInputsContainers) {
+    errorInputContainer.classList.remove('error');
+    errorInputContainer.parentElement.querySelector('p').remove();
+  }
+}
+
+function validateInput(evt) {
+    if (evt.target.value === '') {
+        return;
+    }
+    const { parentElement } = evt.target;
+    const grandParentElement = parentElement.parentElement;
+    const inputValue = evt.target.value.replace(",", ".");
+
+    if (
+        !parentElement.classList.contains("error") &&
+        (isNaN(inputValue) || Number(inputValue) <= 0)
+    ) {
+        const errorTextElement = document.createElement('p');
+        errorTextElement.classList.add('text-red-500');
+        errorTextElement.innerText = "Insira um valor numérico e maior do que zero";
+
+        parentElement.classList.add('error');
+        grandParentElement.appendChild(errorTextElement);
+    } else if (
+        parentElement.classList.contains("error") &&
+        !isNaN(inputValue) &&
+        Number(inputValue) > 0
+    ) {
+        parentElement.classList.remove("error");
+        const errorElement = grandParentElement.querySelector('p');
+        if (errorElement) errorElement.remove();
+    }
+}
+
+for (const formElement of form.elements) {
+    if (formElement.tagName === 'INPUT' && formElement.hasAttribute('name')) {
+        formElement.addEventListener('blur', validateInput);
+    }
+}
 
 form.addEventListener('submit', renderProgression);
+clearFormButton.addEventListener('click', clearform);
